f3cb329caedf195391d32c5dc13f57e13c1ee931
davidalmeida14 <david_almeida_1@hotmail.com>
2020-07-04 18:35:02 -0300

    Ajustes iniciais

--- src/main/java/br/com/rede_social/domain/model/Usuario.java
+++ src/main/java/br/com/rede_social/domain/model/Usuario.java
@@ -2,10 +2,14 @@ package br.com.rede_social.domain.model;
 
 import java.io.Serializable;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
@@ -20,11 +24,22 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class Usuario implements Serializable {
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7534423172268647976L;
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "tb_usuario_seq")
 	@SequenceGenerator(name = "tb_usuario_seq", sequenceName = "tb_usuario_seq", allocationSize = 1)
+	@Column(name = "id_usuario")
 	private Integer id;
 	private String userName;
 	private String email;
 	private String password;
+	
+	@OneToOne(cascade = {CascadeType.PERSIST, CascadeType.REMOVE})
+	@JoinColumn(name = "id_pessoa")
+	private Pessoa pessoa;
+	
 }
--- src/main/java/br/com/rede_social/infrastructure/repository/UsuarioRepository.java
+++ src/main/java/br/com/rede_social/infrastructure/repository/UsuarioRepository.java
@@ -1,5 +1,11 @@
 package br.com.rede_social.infrastructure.repository;
 
-public class UsuarioRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.rede_social.domain.model.Usuario;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
 }
